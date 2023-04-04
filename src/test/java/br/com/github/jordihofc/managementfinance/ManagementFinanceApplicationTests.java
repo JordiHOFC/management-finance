9562f32cbd333331b25ae66ed3459ef3c43ef1af
JordiHOFC <jordyhenrique7@gmail.com>
2023-04-04 12:45:18 -0300

    fix:criando scafold do projeto

---
+++ src/test/java/br/com/github/jordihofc/managementfinance/ManagementFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.github.jordihofc.managementfinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
